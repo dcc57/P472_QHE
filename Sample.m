ba15a97091e9c5786fd212c48da1e18d9e8aa416
David Cyncynates <dcc57@case.edu>
2017-02-20 09:27:12 -0500

    Opening/Closing GPIB Mac
    Yeah

--- Sample.m
+++ Sample.m
@@ -2,4 +2,13 @@ clear
 
 format long
 
-disp('Sample Text')
+%instrhwinfo('visa','ni')
+%% OPEN POWER SUPPLY PROGRAMMER
+gpower = visa('ni', 'ASRL1::INSTR');
+fopen(gpower)
+
+%% CLOSE POWER SUPPLY PROGRAMMER
+
+fclose(gpower);
+delete(gpower);
+clear(gpower);
