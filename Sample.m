25b58bf974a0c3e6cf18a99904be0e8b1be6d186
David Cyncynates <dcc57@case.edu>
2017-02-20 09:31:39 -0500

    Open/Close Lockin/Controller

--- Sample.m
+++ Sample.m
@@ -5,10 +5,18 @@ format long
 %instrhwinfo('visa','ni')
 %% OPEN POWER SUPPLY PROGRAMMER
 gpower = visa('ni', 'GPIB0::22::INSTR');
-fopen(gpower)
+fopen(gpower);
 
 %% CLOSE POWER SUPPLY PROGRAMMER
 
 fclose(gpower);
 delete(gpower);
-clear(gpower);
+
+%% OPEN POWER SUPPLY PROGRAMMER
+glock = visa('ni', 'GPIB0::8::INSTR');
+fopen(glock);
+
+%% CLOSE POWER SUPPLY PROGRAMMER
+
+fclose(glock);
+delete(glock);
